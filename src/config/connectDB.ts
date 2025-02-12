da47db20ae9854d9247e989534ffbd3b80857375
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-12 16:28:26 +0530

    Add type definitions for undici and update database connection logic

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-function connectDB() {
+
+async function connectDB() {
   try {
-    mongoose.connect("mongodb://localhost:27017/E-Commerce");
+    await mongoose.connect("mongodb://localhost:27017/E-CommerceDB", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB Connected");
   } catch (e) {
     console.log("Not working", e);
   }
 }
+
 module.exports = { connectDB };
