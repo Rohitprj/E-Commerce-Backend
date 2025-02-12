a1be81c3c43f3ad94f765cf2816d5cf1c1904629
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-12 17:58:56 +0530

    Update database connection string to use E-CommerceDB

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 function connectDB() {
   try {
-    mongoose.connect("mongodb://localhost:27017/E-Commerce");
+    mongoose.connect("mongodb://localhost:27017/E-CommerceDB");
     console.log("DB Connected");
   } catch (e) {
     console.log("Not working", e);
