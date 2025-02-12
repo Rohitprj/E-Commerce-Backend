42fc8f8c667d236a2b04d2cfd21afd6d5fa9a488
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-12 16:39:07 +0530

    remove

--- src/models/authSchema.ts
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const authSignUp = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
-const SignUp = mongoose.model("authSignUp", authSignUp);
-
-module.exports = SignUp;
