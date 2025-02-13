4c49cbc685d710faaeb8bc187be001cd6d4edf37
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-13 19:51:12 +0530

    Add allCategories function to retrieve and return categories from the database

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,5 +1,9 @@
-async function newCategories() {
+const Categories = require("../models/categoriesSchema");
+
+export async function allCategories(req, res) {
   try {
+    const categories = await Categories.find();
+    res.json(categories);
   } catch (error) {
     console.log(error);
   }
