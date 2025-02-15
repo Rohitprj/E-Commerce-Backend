13343e98c5cd85c6aca00ead27abd2dd3e75bc68
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-15 14:06:30 +0530

    Add Swagger documentation for Expense Tracker API and update package.json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { connectDB } = require("./src/config/connectDB");
 const authRoute = require("./src/routes/authRoute");
 const categoriesRoute = require("./src/routes/categoriesRoute");
 const bodyParser = require("body-parser");
+const swaggerDocs = require("./swagger");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(express.json());
+
+swaggerDocs(app);
 
 const PORT = process.env.PORT || 3009;
 
@@ -20,4 +23,5 @@ app.use("/categories", categoriesRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
+  console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
 });
