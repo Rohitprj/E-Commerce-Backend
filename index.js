7a3f4b71c88442b173c2a4007b3ce0a211fd2d38
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-15 16:09:38 +0530

    Refactor API documentation setup using swagger-jsdoc and swagger-ui-express; remove old swagger.js file

--- index.js
+++ index.js
@@ -3,16 +3,30 @@ const { connectDB } = require("./src/config/connectDB");
 const authRoute = require("./src/routes/authRoute");
 const categoriesRoute = require("./src/routes/categoriesRoute");
 const bodyParser = require("body-parser");
-const swaggerDocs = require("./swagger");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 require("dotenv").config();
 
 const app = express();
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "E-Commerce API",
+      version: "1.0.0",
+      description: "API documentation for E-commerce app",
+    },
+    servers: [{ url: "http://localhost:3009" }],
+  },
+  apis: ["./src/routes/categoriesRoute.js"],
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(bodyParser.json());
 
 app.use(express.json());
-
-swaggerDocs(app);
 
 const PORT = process.env.PORT || 3009;
 
