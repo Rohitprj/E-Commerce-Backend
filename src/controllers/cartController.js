4b8621da4c343ce08c5fdb014b33d98f8dc5f3c9
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-04 17:06:23 +0530

    Remove commented-out code and clean up addToCart function for better readability

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -32,24 +32,6 @@ async function addToCart(req, res) {
       cart.item.push();
     }
     await cart.save();
-    // const productWithQuantity = { ...product.toObject(), quantity };
-
-    // const addCart = new Cart({
-    //   item: [productWithQuantity],
-    // });
-    // console.log("Data", addCart);
-    // await addCart.save();
-
-    // if (addCart) {
-    // }
-    // Cart.updateOne(
-    //   {},
-    //   {
-    //     $push: {
-    //       item: {},
-    //     },
-    //   }
-    // );
 
     res.status(201).json({
       message: "Added to cart successfully",
@@ -64,3 +46,46 @@ async function addToCart(req, res) {
 }
 
 module.exports = { addToCart };
+
+// const Cart = require("../models/cartSchema");
+// const Products = require("../models/productSchema");
+
+// async function addToCart(req, res) {
+//   try {
+//     const { product_id, quantity } = req.body;
+
+//     if (!product_id || !quantity) {
+//       return res
+//         .status(400)
+//         .json({ message: "Product ID and quantity are required!" });
+//     }
+
+//     const product = await Products.findOne({ product_id });
+//     console.log("Product found:", product);
+
+//     if (!product) {
+//       return res.status(400).json({ message: "Product doesn't exist!" });
+//     }
+
+//     const productWithQuantity = { ...product.toObject(), quantity };
+
+//     const addCart = new Cart({
+//       item: [productWithQuantity],
+//     });
+//     console.log("Data", addCart);
+
+//     await addCart.save();
+
+//     res.status(201).json({
+//       message: "Added to cart successfully",
+//       data: addCart,
+//     });
+//   } catch (error) {
+//     console.error("Error in addToCart:", error.message);
+//     return res
+//       .status(500)
+//       .json({ message: "Server error!", error: error.message });
+//   }
+// }
+
+// module.exports = { addToCart };
