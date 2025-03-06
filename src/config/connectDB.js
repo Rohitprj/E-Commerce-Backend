ca30dec6ebce81124da1ab25a08e5de3c9d8740c
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-06 16:42:00 +0530

    Replace hardcoded MongoDB connection string with environment variable for improved security

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 function connectDB() {
   try {
-    mongoose.connect(
-      "mongodb+srv://New_Demo_User:VXrC2WGyxTwyU2sT@cluster0.shqzn.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    mongoose.connect(process.env.URL);
     console.log("DB Connected");
   } catch (e) {
     console.log("Not working", e);
