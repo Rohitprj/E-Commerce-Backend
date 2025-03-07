92c1dcc654a2c1b009e1833d9ae61743ea0b1a63
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-07 14:10:51 +0530

    Rename addToCart function to cartSystem and update logic for user and product retrieval

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,51 +1,64 @@
-const Cart = require("../models/cartSchema");
+const SignUp = require("../models/authSchema");
 const Products = require("../models/productSchema");
 
-async function addToCart(req, res) {
+async function cartSystem(req, res) {
   try {
-    const { product_id, quantity } = req.body;
-
-    if (!product_id || !quantity) {
-      return res
-        .status(400)
-        .json({ message: "Product ID and quantity are required!" });
-    }
-
-    const product = await Products.findOne({ product_id });
-    console.log("Product found:", product);
-
-    if (!product) {
-      return res.status(400).json({ message: "Product doesn't exist!" });
-    }
-    let cart = await Cart.findOne();
-
-    if (!cart) {
-      cart = new Cart({ item: [] });
-    }
-    const cartProdExisting = cart.item.find((data) => {
-      data.product_id.toString() === product_id;
-    });
-
-    if (cartProdExisting) {
-      cartProdExisting.quantity += quantity;
-    } else {
-      cart.item.push();
-    }
-    await cart.save();
-
-    res.status(201).json({
-      message: "Added to cart successfully",
-      data: cart,
-    });
+    const { userId, prodId } = req.params.id;
+    const takeUserId = SignUp.findById(userId);
+    const takeProdId = Products.findById(prodId);
   } catch (error) {
-    console.error("Error in addToCart:", error.message);
-    return res
-      .status(500)
-      .json({ message: "Server error!", error: error.message });
+    console.log(error);
   }
 }
+module.exports = { cartSystem };
+// const Cart = require("../models/cartSchema");
+// const Products = require("../models/productSchema");
 
-module.exports = { addToCart };
+// async function addToCart(req, res) {
+//   try {
+//     const { product_id, quantity } = req.body;
+
+//     if (!product_id || !quantity) {
+//       return res
+//         .status(400)
+//         .json({ message: "Product ID and quantity are required!" });
+//     }
+
+//     const product = await Products.findOne({ product_id });
+//     console.log("Product found:", product);
+
+//     if (!product) {
+//       return res.status(400).json({ message: "Product doesn't exist!" });
+//     }
+//     let cart = await Cart.findOne();
+
+//     if (!cart) {
+//       cart = new Cart({ item: [] });
+//     }
+//     const cartProdExisting = cart.item.find((data) => {
+//       data.product_id.toString() === product_id;
+//     });
+
+//     if (cartProdExisting) {
+//       cartProdExisting.quantity += quantity;
+//     } else {
+//       cart.item.push();
+//     }
+//     await cart.save();
+
+//     res.status(201).json({
+//       message: "Added to cart successfully",
+//       data: cart,
+//     });
+//   } catch (error) {
+//     console.error("Error in addToCart:", error.message);
+//     return res
+//       .status(500)
+//       .json({ message: "Server error!", error: error.message });
+//   }
+// }
+
+// module.exports = { addToCart };
 
 // const Cart = require("../models/cartSchema");
 // const Products = require("../models/productSchema");
