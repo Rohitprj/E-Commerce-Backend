dda05939ad93d7721214b2933e86acda9ae2cc87
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-10 12:50:22 +0530

    Add cart item schema to define product ID and quantity requirements

--- src/models/cartSystemSchema.js
+++ src/models/cartSystemSchema.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose");
+
+const cartItemSchema = new Schema({
+  prodId: { type: Types.ObjectId, ref: "products", required: true },
+  quantity: { type: Number, required: true, min: 1 },
+});
+
+const cartSchema = new Schema({});
+module.exports = {};
