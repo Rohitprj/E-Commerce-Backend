525e4adc92881a6a279171ab6a70cfe3c70b51a8
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-20 18:24:21 +0530

    Enhance order schema with additional fields and improve structure

--- src/models/orderSchema.js
+++ src/models/orderSchema.js
@@ -1,8 +1,32 @@
-const { Mongoose } = require("mongoose");
+const { Mongoose, Types } = require("mongoose");
 const { AddressBasedSchema } = require("./address.order");
 const { cartItemSchema } = require("./cartSystemSchema");
 
-const orderSchema = new Mongoose.Schema({
+const OrderSchema = new Mongoose.Schema({
   address: { type: AddressBasedSchema, required: true },
   items: { type: [cartItemSchema], required: true },
+  uid: { type: Types.ObjectId, ref: "users", required: true },
+  shippingFee: { type: Number, default: 0, min: 0 },
+  status: {
+    type: String,
+    enum: [
+      "Placed",
+      "Confirmed",
+      "Shipped",
+      "OFD",
+      "Delivered",
+      "Cancelled",
+      "Returned",
+    ],
+    default: "Placed",
+  },
+  orderTimeline: {
+    placedAt: { type: Date, default: Date.now, required: true },
+    confirmedAt: { type: Date },
+    shippedAt: { type: Date },
+    deliveredAt: { type: Date },
+    cancelledAt: { type: Date },
+    returnedAt: { type: Date },
+  },
 });
+module.exports = { OrderSchema };
