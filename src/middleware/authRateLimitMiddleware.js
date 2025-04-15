4a30aa12e434ee7cea938df420ee3299d1109de9
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-15 13:18:54 +0530

    Fix typo in logIn rate limiter configuration

--- src/middleware/authRateLimitMiddleware.js
+++ src/middleware/authRateLimitMiddleware.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const logInLimiter = rateLimit({
-  windowsMs: 15 * 60 * 1000, // for 15 minutes
+  windowMs: 15 * 60 * 1000, // for 15 minutes
   max: 7, // max 7 attempts
   message: "Too many login attempts from this IP, please try again later",
   standardHeaders: true,
