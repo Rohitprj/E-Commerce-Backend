adc0b76d89db7b4c9ebff0efd2021323fb6b8535
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-24 16:25:41 +0530

    Update trust proxy setting and enhance rate limiting configuration

--- src/middleware/authRateLimitMiddleware.js
+++ src/middleware/authRateLimitMiddleware.js
@@ -2,10 +2,11 @@ const rateLimit = require("express-rate-limit");
 
 const logInLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // for 15 minutes
-  max: 7, // max 7 attempts
+  max: 20, // max 20 attempts
   message: "Too many login attempts from this IP, please try again later",
   standardHeaders: true,
   legacyHeaders: false,
+  keyGenerator: (req) => req.ip, // safe usage with proper trust proxy setting
 });
 
 const signupLimiter = rateLimit({
@@ -13,6 +14,7 @@ const signupLimiter = rateLimit({
   max: 10, // Allow 10 signups per IP per hour
   message:
     "Too many accounts created from this IP, please try again after an hour",
+  keyGenerator: (req) => req.ip, // safe usage with proper trust proxy setting
 });
 
 module.exports = { logInLimiter, signupLimiter };
